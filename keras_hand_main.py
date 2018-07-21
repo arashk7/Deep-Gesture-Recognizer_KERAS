c1ea802be77557ee68effc53e38f36cd649d0fe7
arashk7 <ahmad.karambakhsh@gmail.com>
2018-07-21 08:52:29 +0800

    edited

--- keras_hand_main.py
+++ keras_hand_main.py
@@ -8,20 +8,7 @@ import h5py
 from test_keras.keras_hand_train import ak_train
 from test_keras.keras_hand_test import ak_test
 
-#ak_train(300)  # 38
 
 p = ak_test()
 
 
-
-# min=100
-# selected_index=0
-# for i in range(25,55):
-#     ak_train(i)
-#     p=ak_test()
-#     if p<min:
-#         min=p
-#         selected_index=i
-#         print (min," with epoch ",selected_index)
-#
-# print("The minimum error",min ,"is appeared with: ", selected_index," epochs")
